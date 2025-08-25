372a890cdfa63be807c0352e6a7c06817b915130
Alvick Thelusma <thelualv000@warren.k12.in.us>
2025-08-25 08:13:08 -0400

    Initial commit

---
+++ Op.py
@@ -0,0 +1,18 @@
+
+Number1 = 14
+Number2 = 24
+
+print(Number1 + Number2)
+
+print(Number1 - Number2)
+
+print(Number1 * Number2)
+
+print(Number1 / Number2)
+
+print(Number1 // Number2)
+
+print(Number1 % Number2)
+
+print(Number1 ** Number2)
+
